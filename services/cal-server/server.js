5704700340552977003190d512c6c76d054228e7
Pedro Dias <pmds3000@hotmail.com>
2019-04-21 22:16:01 +0100

    generating JSON FILE

--- services/cal-server/server.js
+++ services/cal-server/server.js
@@ -3,14 +3,12 @@ const RAW_DATA_PATH = DATA_PATH + "/raw_data";
 const ZIPPED_DATA_PATH = DATA_PATH + "/compressed_data";
 const EDGE_TAG = "_edges_";
 const NODE_X_Y_TAG = "_nodes_X_Y_";
-const NODE_LAT_LONG_TAG = "nodes_lat_lon_";
+const NODE_LAT_LONG_TAG = "_nodes_lat_lon_";
 const TAG_TAG= "_tags_";
 
 let fs = require("fs");
 let targz = require('targz');
-var async = require("async");
-
-
+let async = require("async");
 
 const readRawData = function () {
   
@@ -31,10 +29,7 @@ const readRawData = function () {
     }   
 
     const generateJson = function (folderRoot, folderCurrent) {
-        let json =  {
-            edges: [],
-            nodes: [],
-        }
+       
 
         const generateFilePath = function (path, folderRoot, folderCurrent, tag) {
             return path + '/' + folderRoot +  tag + folderCurrent + ".txt";
@@ -61,31 +56,134 @@ const readRawData = function () {
               }
             });
         }
-        const readEdges = function (folder, cb) {
-            let path= generateFilePath(RAW_DATA_PATH, folderRoot, folder, EDGE_TAG );
+
+
+        const readEdges = function (rootPath, folder, cb) {
+            let path= generateFilePath(rootPath, folderRoot, folder, EDGE_TAG );
 
             let input = fs.createReadStream(path);
+            
+            let edges = [];
+            let lineCount = 0;
 
             const lineProcessing = function (data, cb) {
-               // console.log(data); 
+                if(lineCount != 0 && !data.includes("oid") && !data.includes("size")) {
+                  let result = data.split(", ")
+                  let n1 = result[0].split("(")[1];      
+                  let n2 = result[1].split(")")[0]; 
+                  edges.push({n1,n2});
+                }  
+                lineCount++;
                 cb();
             }
 
-            readLines(input, function(data, endend)  {
-                lineProcessing(data, function () {
-
-                })
+            readLines(input, function(data)  {
+                lineProcessing(data, function() {});
             });
 
+            input.on('close', function() {
+                    console.log("Called ");
+                    cb(null, edges);
+              });
         };
-        const readNodesXY = function (path, cb) {
-            cb();
+
+        const readNodesXY = function (rootPath, folder, cb) {
+            let path= generateFilePath(rootPath, folderRoot, folder, NODE_X_Y_TAG );
+
+            let input = fs.createReadStream(path);
+            
+            let nodes = [];
+            let lineCount = 0;
+
+            const lineProcessing = function (data) {
+                if(lineCount != 0 && !data.includes("oid") && !data.includes("size")) {
+                  let result = data.split(", ");
+                  let nodeId = parseInt(result[0].split("(")[1]);      
+                  let x = result[1];
+                  let y = result[2].split(")")[0];
+                  nodes.push({nodeId, x, y});
+                }  
+                lineCount++;
+            }
+
+            readLines(input, function(data)  {
+                lineProcessing(data, function (){});
+            });
+
+            input.on('close', function() {
+                    cb(null,nodes);
+              });
         };
-        const readNodesLatLong = function (path, cb) {
-            cb();
+        
+        const readNodesLatLong = function (rootPath, folder, cb) {
+            let path= generateFilePath(rootPath, folderRoot, folder, NODE_LAT_LONG_TAG );
+
+            let input = fs.createReadStream(path);
+            
+            let nodes = [];
+            let lineCount = 0;
+
+            const lineProcessing = function (data) {
+                if(lineCount != 0 && !data.includes("oid") && !data.includes("size")) {
+                  let result = data.split(", ")
+                  let nodeId = parseInt(result[0].split("(")[1]);      
+                  let lat = result[1];
+                  let long = result[2].split(")")[0];
+                  nodes.push({nodeId, lat, long});
+                }  
+                lineCount++;
+            }
+
+            readLines(input, function(data)  {
+                lineProcessing(data, function (){});
+            });
+
+            input.on('close', function() {
+                    cb(null,nodes);
+              });
         }; 
-        const readTags = function (path, cb) {
-            cb();
+        const readTags = function (rootPath, folder, cb) {
+            let path= generateFilePath(rootPath, folderRoot, folder, TAG_TAG );
+
+            let input = fs.createReadStream(path);
+            
+            let tags = [];
+            let lineCount = 0;
+            let ignoreLine = false;
+            let name = "";
+            let nodes = [];
+
+            const pushData = function() {
+                if(nodes.length > 0) {
+                 tags.push({name, nodes});
+                 nodes = [];
+                }
+            }
+
+            const lineProcessing = function (data) {
+                if(lineCount !== 0 && !data.includes("oid") && !data.includes("size")) {
+                   if(ignoreLine){
+                       ignoreLine = false;
+                   } else if (isNaN(data)) {
+                       ignoreLine = true;
+                       pushData();
+                       name = data;
+                   } else {
+                       let nodeID = parseInt(data);
+                       nodes.push(nodeID);
+                   }
+                }  
+                lineCount++;
+            }
+
+            readLines(input, function(data)  {
+                lineProcessing(data, function (){});
+            });
+
+            input.on('close', function() {
+                pushData();        
+                cb(null,tags);
+              });
         };
         
         let folderRootPath = RAW_DATA_PATH + "/" + folderRoot ;
@@ -97,31 +195,54 @@ const readRawData = function () {
                 let path = folderRootPath + "/" + folder;
     
                 if(fs.lstatSync(path).isDirectory())  {
-                  
-                  let nodesXYFile = generateFilePath(path, folderRoot, folder, NODE_X_Y_TAG );
-                  let nodesLatLongFile = generateFilePath(path, folderRoot, folder, NODE_LAT_LONG_TAG );
-                  let tags = generateFilePath(path, folderRoot, folder, TAG_TAG );
-
 
                   async.series (
                      [
                        function (cb) {
-                        readEdges(folder,cb);
+                        readEdges(path,folder,cb);
+                       },
+                       function(cb)  {
+                        readNodesXY(path, folder, cb);
                        },
                        function(cb) {
-                        readNodesXY(nodesXYFile,cb)
+                        readNodesLatLong(path, folder,cb);
                        },
                        function(cb) {
-                        readNodesLatLong(nodesLatLongFile,cb)
-                       },
-                       function(cb) {
-                        readTags(tags,cb);
+                        readTags(path, folder, cb);
                        }
-                     ], function(err) {
+                     ], function(err, results) {
                            if(err) {
                                console.log(err);
                            }
                            else {
+                               //TODO mergeResults
+                               let edges = results[0];
+                               let nodesXY = results[1];
+                               let nodesLatLong = results[2];
+                               let tags = results[3];
+                               let nodes = [];
+                               
+                                nodesXY.map( item => {
+                                        let node = nodesLatLong.find( function (element) {
+                                          return element.nodeId == item.nodeId;
+                                        })
+
+                                        node.x = item.x;
+                                        node.y = item.y;
+                                        
+                                        node.tags = [];
+
+                                        tags.forEach( function(element) {
+                                            if(element.nodes.indexOf(node.nodeId) > -1)
+                                                node.tags.push(element.name);
+                                        }) || [];
+
+                                        nodes.push(node);
+                                        
+
+                                    }   
+                                )                       
+                                
                                console.log("JSON Object Generacted");
                            }
                        }
@@ -145,8 +266,7 @@ const readRawData = function () {
             let path = RAW_DATA_PATH + "/" + folder;
 
             if(fs.lstatSync(path).isDirectory() &&  folder !== ".gitignore" )
-                console.log(folder);
-                generateCompressedFile(folder);
+               // generateCompressedFile(folder);
                 generateJson(folder); 
             })
         }
@@ -155,15 +275,16 @@ const readRawData = function () {
 
 
 const makeDir = function (path , cb) {
-    fs.mkdir(path, {recursive: true},  (err,folder) => {
-        if(err) {
-            console.warn("makeDir ", "path:"+ path, err);
-             cb(true);
-        }
-        else{
-            console.log("Folder create at " +  path);
-            cb(false);
-        }
+    if(!fs.existsSync(path))
+        fs.mkdir(path, {recursive: true},  (err,folder) => {
+            if(err) {
+                console.warn("makeDir ", "path:"+ path, err);
+                cb(true);
+            }
+            else {
+                console.log("Folder create at " +  path);
+                cb(false);
+            }
 
     })
 }
